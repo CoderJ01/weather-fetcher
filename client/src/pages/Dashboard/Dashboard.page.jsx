63b0a603eca07dd909d1e6e5f9a5f32a6d9d1c94
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-19 19:58:56 -0700

    establish classnames for dashboard

--- client/src/pages/Dashboard/Dashboard.page.jsx
+++ client/src/pages/Dashboard/Dashboard.page.jsx
@@ -2,7 +2,37 @@
 import React from 'react';
 
 const Dashboard = () => {
-    return <>Dashboard</>
+    return (
+        <>
+        <div className='weather'>
+            <div className='search'>
+                <div className='input'>
+
+                </div>
+                <div className='popular'>
+
+                </div>
+            </div>
+            <div className='forecast'>
+                <div className='current'>
+
+                </div>
+                <div className='future'>
+                    <div className='day'>
+
+                    </div>
+                    {/* next four days */}
+                </div>
+                <div className='most-searched'>
+
+                </div>
+            </div>
+        </div>
+        <div className='history'>
+
+        </div>
+        </>
+    );
 }
 
 export default Dashboard;
