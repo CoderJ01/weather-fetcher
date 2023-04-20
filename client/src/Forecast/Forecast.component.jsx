33e409c13bd7cece249a9d9918eeb641707aa19c
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-19 21:07:54 -0700

    create Forecast component

---
+++ client/src/Forecast/Forecast.component.jsx
@@ -0,0 +1,23 @@
+// React 
+import React from 'react';
+
+const Forecast = () => {
+    return (
+        <div className='forecast'>
+            <div className='f-current'>
+
+            </div>
+            <div className='f-future'>
+                <div className='ff-day'>
+
+                </div>
+                {/* next four days */}
+            </div>
+            <div className='f-most-searched'>
+
+            </div>
+        </div>
+    );
+}
+
+export default Forecast;
