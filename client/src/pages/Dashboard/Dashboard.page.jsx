34adb5382d3146b9f23883974472f7d77b54b4b4
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-20 14:22:27 -0700

    search button functionality

--- client/src/pages/Dashboard/Dashboard.page.jsx
+++ client/src/pages/Dashboard/Dashboard.page.jsx
@@ -7,8 +7,26 @@ import './Dashboard.style.css';
 // components
 import Forecast from '../../components/Forecast/Forecast.component';
 
+// other imports
+import axios from 'axios';
+
 const Dashboard = () => {
     const [input, setInput] = useState('');
+    const [weatherInfo, setWeatherInfo] = useState([]);
+
+    const handleSearchSubmit = () => {
+        const fetchCurrentWeather = async () => {
+            try {
+                const response = await axios.request(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
+                setWeatherInfo(response);
+                console.log(weatherInfo);
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+        fetchCurrentWeather();
+    }
 
     return (
         <>
@@ -16,7 +34,7 @@ const Dashboard = () => {
             <div className='search'>
                 <div className='search-input'>
                     <input value={input} onChange={(e) => {setInput(e.target.value)}}></input>
-                    <button>Search</button>
+                    <button onClick={handleSearchSubmit}>Search</button>
                 </div>
                 <div className='search-popular'>
 
