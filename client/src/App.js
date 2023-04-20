b3b62e93b00756f1bb035b671d6c570ee535b2bb
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-19 17:48:32 -0700

    test request for Open Weather API using axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
+// React
+import { useEffect } from 'react';
+
 // CSS
 import './App.css';
 
+// other imports
+import axios from 'axios';
+
 function App() {
+
+	useEffect(() => {
+		const fetchCurrentWeather = async () => {
+			try {
+				const response = await axios.request(`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&&units=imperial&lon=-94.04&exclude=hourly&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
+				console.log(response);
+			}
+			catch(error) {
+				console.log(error);
+			}
+		}
+		fetchCurrentWeather();
+	}, []);
+
 	return (
 		<div className='App'>
 			
