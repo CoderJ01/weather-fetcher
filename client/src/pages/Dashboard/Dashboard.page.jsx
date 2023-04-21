7c882dd84bcf13a33a486ec1ef6170467f25aa67
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-20 19:42:56 -0700

    fetch info for daily forcast

--- client/src/pages/Dashboard/Dashboard.page.jsx
+++ client/src/pages/Dashboard/Dashboard.page.jsx
@@ -21,18 +21,22 @@ const Dashboard = () => {
     const [windSpeed, setWindSpeed] = useState(0);
     const [humidity, setHumidity] = useState(0);
     const [fethced, setFetched] = useState(false);
+    const [daily, setDaily] = useState();
 
     const handleSearchSubmit = useCallback(() => {
         if(input !== '') {
             const fetchCurrentWeather = async () => {
                 try {
                     const response = await axios.request(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
+                    const response_5day = await axios.request(`https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&&units=imperial&lon=${response.data.coord.lon}&exclude=hourly&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
                     setCity(response.data.name);
                     setTemperature(response.data.main.temp);
                     setWindSpeed(response.data.wind.speed);
                     setHumidity(response.data.main.humidity);
+                    setDaily(response_5day.data.daily);
                     setFetched(true);
                     console.log(response.data);
+                    console.log(response_5day.data.daily)
                 }
                 catch(error) {
                     console.log(error);
@@ -47,12 +51,15 @@ const Dashboard = () => {
             const fetchCurrentWeather = async () => {
                 try {
                     const response = await axios.request(`https://api.openweathermap.org/data/2.5/weather?q=${selection}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
+                    const response_5day = await axios.request(`https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&&units=imperial&lon=${response.data.coord.lon}&exclude=hourly&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
                     setCity(response.data.name);
                     setTemperature(response.data.main.temp);
                     setWindSpeed(response.data.wind.speed);
                     setHumidity(response.data.main.humidity);
                     setFetched(true);
+                    setDaily(response_5day.data.daily);
                     console.log(response.data);
+                    console.log(response_5day.data.daily)
                 }
                 catch(error) {
                     console.log(error);
@@ -92,6 +99,7 @@ const Dashboard = () => {
                 wind={windSpeed}
                 humidity={humidity}
                 fetched={fethced}
+                daily={daily}
             />
         </div>
         <div className='history'>
