003b40134c819640a2b212efa6e23b2e40d7050e
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-21 14:17:52 -0700

    connect to MongoDB database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 // Express.js
 const express = require('express');
 
+// MongoDB
+const connectDB = require('./config/connection');
+
 // other imports
 const cors = require('cors');
 require('colors');
@@ -8,6 +11,8 @@ require('dotenv').config();
 
 const app = express();
 const PORT = 3001;
+
+connectDB();
 
 app.use(express.json);
 app.use(cors({
