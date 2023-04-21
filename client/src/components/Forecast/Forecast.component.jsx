de510f3403ccd631f057fdba9863e69034dcd4e7
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-20 19:17:45 -0700

    configure default display if no weather data is fetched

--- client/src/components/Forecast/Forecast.component.jsx
+++ client/src/components/Forecast/Forecast.component.jsx
@@ -4,17 +4,36 @@ import React from 'react';
 // CSS 
 import './Forecast.style.css';
 
-const Forecast = ({ city, temp, wind, humidity }) => {
+const Forecast = ({ city, temp, wind, humidity, fetched }) => {
     return (
         <div className='forecast'>
             <div className='f-current'>
-                <h1>{city}</h1>
-                <br/>
-                <p>Temp: {((temp - 273.15) * (9/5) + 32).toFixed(2)}°F</p>
-                <br/>
-                <p>Wind: {wind} mph</p>
-                <br/>
-                <p>Humidity: {humidity}%</p>
+            {
+                !fetched ? 
+                (
+                    <>
+                    <h1>City</h1>
+                    <br/>
+                    <p>Temp:</p>
+                    <br/>
+                    <p>Wind:</p>
+                    <br/>
+                    <p>Humidity:</p>
+                    </>
+                ) : 
+                (
+                    <>
+                    <h1>{city}</h1>
+                    <br/>
+                    <p>Temp: {((temp - 273.15) * (9/5) + 32).toFixed(2)}°F</p>
+                    <br/>
+                    <p>Wind: {wind} mph</p>
+                    <br/>
+                    <p>Humidity: {humidity}%</p>
+                    </>
+                )
+            }
+                
             </div>
             <div className='f-future'>
                 <div className='ff-day'>
