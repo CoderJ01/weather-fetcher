f83eb7782d2088f52c542231329296bf86991170
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-21 13:41:44 -0700

    clean

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,3 @@
-// React
-import { useEffect } from 'react';
-
 // CSS
 import './App.css';
 
@@ -10,23 +7,7 @@ import Dashboard from './pages/Dashboard/Dashboard.page';
 // components
 import Header from './components/Header/Header.component';
 
-// other imports
-import axios from 'axios';
-
 function App() {
-
-	useEffect(() => {
-		const fetchCurrentWeather = async () => {
-			try {
-				const response = await axios.request(`https://api.openweathermap.org/data/2.5/weather?q=${'miami'}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`);
-				console.log(response);
-			}
-			catch(error) {
-				console.log(error);
-			}
-		}
-		fetchCurrentWeather();
-	}, []);
 
 	return (
 		<div className='App'>
