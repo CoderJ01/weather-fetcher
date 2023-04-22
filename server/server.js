1e694d88a5243dad37f2c499382d4eed2308b1c7
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-22 13:25:17 -0700

    add origin

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: ['http://localhost:3000', process.env.DEPLOYED_FRONTEND]
 }));
 
 app.use(routes);
