b46863ba618c4d02fbeb446c9ba13907bb986d40
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-21 21:55:35 -0700

    fetch searches

--- client/src/components/PopularSearches/PopularSearches.component.jsx
+++ client/src/components/PopularSearches/PopularSearches.component.jsx
@@ -1,13 +1,38 @@
 // React
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 // CSS
 import './PopularSearches.style.css';
 
+// URL
+import { baseURL } from '../../utils/urls';
+
+// other imports
+import axios from 'axios';
+
 const PopularSearches = () => {
+    const [topSeaches, setTopSearches] = useState([]);
+    
+    console.log(topSeaches);
+
+    const fetchCities = useCallback(async () => {
+        try {
+            const response = await axios.get(`${baseURL}/search-history`);
+            console.log(response.data);
+            setTopSearches(response.data);
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }, [setTopSearches]);
+
+    useEffect(() => {
+        fetchCities();
+    }, [fetchCities]);
+
     return (
         <div className='popular-searches'>
-            <h2>Top Three Searches</h2>
+            <h2>Top Searches</h2>
             <p>1. City</p>
             <p>2. City</p>
             <p>3. City</p>
