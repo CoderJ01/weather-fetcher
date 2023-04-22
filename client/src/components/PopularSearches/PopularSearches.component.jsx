6579fb22d1f53a1d2aa030fc38e893486d37a288
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-21 20:53:54 -0700

    dummy data for popular searhces in jsx

--- client/src/components/PopularSearches/PopularSearches.component.jsx
+++ client/src/components/PopularSearches/PopularSearches.component.jsx
@@ -2,7 +2,14 @@
 import React from 'react';
 
 const PopularSearches = () => {
-    return<>Popular Searches</>
+    return (
+        <div className='popular-searches'>
+            <h2>Top Three Searches</h2>
+            <p>1. City</p>
+            <p>2. City</p>
+            <p>3. City</p>
+        </div>
+    );
 }
 
 export default PopularSearches;
