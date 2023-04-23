20a4c49e5276f9f335269ca8aba2622f77102511
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-22 17:22:27 -0700

    add className to Loader

--- client/src/components/Loader/Loader.component.jsx
+++ client/src/components/Loader/Loader.component.jsx
@@ -4,19 +4,20 @@ import { Oval } from  'react-loader-spinner';
 
 const Loader = () => {
     return (
-        <Oval
-            height={60}
-            width={60}
-            color='rgb(32, 43, 62)'
-            wrapperStyle={{}}
-            wrapperClass=''
-            visible={true}
-            ariaLabel='oval-loading'
-            secondaryColor='rgb(63, 162, 219)'
-            strokeWidth={2}
-            strokeWidthSecondary={2}
-
-        />
+        <div className='loader'>
+            <Oval
+                height={60}
+                width={60}
+                color='rgb(32, 43, 62)'
+                wrapperStyle={{}}
+                wrapperClass=''
+                visible={true}
+                ariaLabel='oval-loading'
+                secondaryColor='rgb(63, 162, 219)'
+                strokeWidth={2}
+                strokeWidthSecondary={2}
+            />
+        </div>
     );
 }
 
