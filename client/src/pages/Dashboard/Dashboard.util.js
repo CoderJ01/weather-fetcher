3158ec38ee3b229fa5dd1273eee5ddc1c16742b6
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-18 14:58:21 -0700

    create new util function

--- client/src/pages/Dashboard/Dashboard.util.js
+++ client/src/pages/Dashboard/Dashboard.util.js
@@ -1,1 +1,11 @@
 export const popularCites = ['Austin', 'Chicago', 'New York', 'Orlando', 'San Francisco', 'Seattle', 'Denver', 'Atlanta'];
+
+export const checkPopularCity = (input) => {
+    for(let i = 0; i < popularCites.length; i++) {
+        if(input.toLowerCase() === popularCites[i].toLowerCase()) {
+            alert(`Use button for ${popularCites[i]}`);
+            return true;
+        }
+    }
+    return false;
+}
